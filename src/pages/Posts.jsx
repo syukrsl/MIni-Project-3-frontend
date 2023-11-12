4152e5b38a6ddd5797df6569df93a326d7b04eb9
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-12 21:01:11 +0800

    Add posts page functionality

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,27 @@
-function Posts() {
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+function Post() {
+    const [post, setPost] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/posts/${id}`)
+            .then(res => res.json())
+            .then(data => setPost(data.data))
+    })
+
     return (
-        <h1>All Posts</h1>
+        <>
+            <h1>{post.title}</h1>
+            <p>{post.description}</p>
+            <img src={post.image} />
+            <div>
+                <Link to={`/posts/${id}/edit`}>Edit Post</Link>
+                <Link to="/posts">Back to Posts</Link>
+            </div>
+        </>
     )
 }
 
-export default Posts;
+export default Post;
